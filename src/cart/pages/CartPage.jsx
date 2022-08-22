27f2fc7957ed2548920bf38907c831507273c8c2
tbls <tbls.0312@gmail.com>
2022-08-22 01:12:13 -0500

    1 file modified, 1 added

--- src/cart/pages/CartPage.jsx
+++ src/cart/pages/CartPage.jsx
@@ -1,7 +1,17 @@
+import Accordion from 'react-bootstrap/Accordion';
+import { CartItem } from '../components/CartItem';
+
 export const CartPage = () => {
+  const arr = [1, 2, 3, 4, 5];
+
   return (
     <>
       <h1 className="my-3 text-center">Cart Page</h1>
+      <Accordion defaultActiveKey={['0']}>
+        {arr.map((ar, index) => (
+          <CartItem key={ar} />
+        ))}
+      </Accordion>
     </>
   );
 };
