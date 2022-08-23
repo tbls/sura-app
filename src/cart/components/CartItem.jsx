09d5a68806f5ec8cc74685a4dd3775637c3a23da
tbls <tbls.0312@gmail.com>
2022-08-22 21:57:14 -0500

    1 file modified

--- src/cart/components/CartItem.jsx
+++ src/cart/components/CartItem.jsx
@@ -1,18 +1,17 @@
-import Card from 'react-bootstrap/Card';
+import { Button } from 'react-bootstrap';
 
 export const CartItem = ({ product, index }) => {
+  const { amount, name, color, size } = product;
 
-	const { amount, name, color, size } = product
-
-	return (
-		<Card>
-
-				<div> {index+1} </div>
-				<div> {amount} </div>
-				<div> {name} </div>
-				<div> {color} </div>
-				<div> {size} </div>
-
-		</Card>
-	);
+  return (
+    <div className="d-flex justify-content-between border rounded border-primary mb-2 p-2">
+      <div className="fs-3">
+        {amount} {name} {color} {size}
+      </div>
+      <div>
+        <Button variant="danger small">Delete</Button>
+        <Button  className="ms-1">Edit</Button>
+      </div>
+    </div>
+  );
 };
